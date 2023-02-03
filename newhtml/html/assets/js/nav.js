bb8fea8e6de1b6e75d4af4bf7a02ebda05eaf069
wei.xia <wei.xia@myt11.com>
2023-02-03 13:59:00 +0800

    add search

--- newhtml/html/assets/js/nav.js
+++ newhtml/html/assets/js/nav.js
@@ -1,7 +1,10 @@
+
 $('.warpper .page-toc ul ul li a').on('click', function () {
     $('.warpper .page-toc ul ul li a').removeClass('my-active')
     $(this).addClass('my-active')
 })
+
+
 
 let isMobile = $(window).width() <= 479
 if(isMobile) {
@@ -13,6 +16,35 @@ if(isMobile) {
     $('body').click(function() {
         $('.warpper .page-toc').hide()
     });
+}else{
+    let html =  $('#mynavlist').html()
+    let arr = []
+    $('#mynavlist > li a').each(function(i,el) {
+        arr.push({
+           title: $(this).text(),
+           href:$(this).attr('href')
+        })
+    })
+    console.log(arr);
+    $('#mynavlist').prepend(`<li class="nav_search_li"><input id="nav_search" placeholder="请输入导航名称" onchange="inputchange()"></li>`)
+
+    function inputchange(){
+        let val = $('#nav_search').val()
+        if(val){
+            let lis = `<li class="nav_search_li"><input id="nav_search" placeholder="请输入导航名称" value="${val}" onchange="inputchange()"></li>`
+            for (let i = 0; i < arr.length; i++) {
+                const item = arr[i];
+                if(item.title.includes(val)){
+                    lis += `<li><a href="${item.href}">${item.title}</a></li>`
+                }
+            }
+            $('#mynavlist').html(lis)
+        }else{
+            $('#mynavlist').html(html)
+            $('#mynavlist').prepend(`<li class="nav_search_li"><input id="nav_search" placeholder="请输入导航名称" onchange="inputchange()"></li>`)
+        }
+    }
+   
 }
 $('.logo').on('click', function (event) {
     event.stopPropagation();
